d238f9ed75508f36d78645fb65bb739552f90748
Javier Mesa <fjmh89@icloud.com>
2023-03-23 16:17:39 +0100

    implement createUser usecase

---
+++ application/user/createUser.usecase.ts
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { useLocalStorageUser } from '@infrastructure/localStorage';
+import { UserAdapter } from '@services/user.adapter';
+import { CreateUserPayload, UserService } from '@services/user.port';
+
+class CreateUserUseCase {
+  private userService: UserService;
+
+  constructor() {
+    this.userService = new UserAdapter();
+  }
+
+  createUser(payload: CreateUserPayload) {
+    return this.userService.createUser(payload);
+  }
+}
+
+function useCreateUser({ blockchain, address }: CreateUserPayload) {
+  const [user, setUser] = useLocalStorageUser();
+
+  const [request, { loading, error, data }] =
+    new CreateUserUseCase().createUser({ blockchain, address });
+
+  useEffect(() => {
+    request?.();
+  }, [request]);
+
+  useEffect(() => {
+    if (data) {
+      setUser(data);
+    }
+  }, [data, setUser]);
+
+  return {
+    user,
+    loading,
+    error,
+  };
+}
+
+export { useCreateUser };
